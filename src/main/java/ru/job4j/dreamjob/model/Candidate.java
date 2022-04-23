63872337869b8e3541c7a91594f13073f70b8f69
Quertte <a.injen90@gmail.com>
2022-04-23 18:47:58 +0300

    3. Формы. Загрузка файла на сервер. [#504855 #292507]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class Candidate {
     private int id;
     private String name;
+    private byte[] photo;
 
     public Candidate(int id, String name) {
         this.id = id;
@@ -25,6 +26,14 @@ public class Candidate {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
     }
 
     @Override
